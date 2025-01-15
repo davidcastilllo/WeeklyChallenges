f555230b849e916fc7c8ad721db5292b1e66ee4e
david castillo <seth.castillo4@outlook.com>
2025-01-14 19:40:22 -0600

    Saved changes

--- ChallengesWithTestsMark8/obj/Release/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Release/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1535a7e6558d0bc2d3d84e3d73fd4e4047615b30")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
